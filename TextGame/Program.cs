45be823e4bf033e9d1262957af3f06f0cbdfe3c4
Stacie <Stacie.Straub@gmail.com>
2015-10-31 22:18:20 -0600

    Updates

--- TextGame/Program.cs
+++ TextGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,87 @@ namespace TextGame
 {
     class Program
     {
+        public static void parseRooms(string filename, ref List<Room> RoomList)
+        {
+            Room ReadRoom = new Room();
+            string row;
+            int val = 0;
+            if (File.Exists(filename) == true)
+            {
+                Console.WriteLine("File Exists");
+                StreamReader read = new StreamReader(filename);
+
+
+                while (read.ReadLine() != null)
+                {
+                    while (val < 8)
+                    {
+                        row = read.ReadLine();
+
+                        switch (val)
+                        {
+                            case 0:
+                                {
+                                    ReadRoom.id = Convert.ToInt32(row);
+                                    val++;
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    ReadRoom.RoomName = row;
+                                    val++;
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    ReadRoom.Description = row;
+                                    val++;
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    ReadRoom.otherItems = row;
+                                    val++;
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    ReadRoom.northid = Convert.ToInt32(row);
+                                    val++;
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    ReadRoom.eastid = Convert.ToInt32(row);
+                                    val++;
+                                    break;
+                                }
+                            case 6:
+                                {
+                                    ReadRoom.southid = Convert.ToInt32(row);
+                                    val++;
+                                    break;
+                                }
+                            case 7:
+                                {
+                                    ReadRoom.westid = Convert.ToInt32(row);
+                                    RoomList.Add(ReadRoom);
+                                    Console.WriteLine("Added Room: " + ReadRoom.RoomName);
+                                    val++;
+                                    break;
+                                }
+                        };
+                    }
+                    val = 0;
+                }
+            }
+            else
+            {
+                Console.WriteLine("File Doesn't exist");
+                return;
+            }
+        }
+
         static void Main(string[] args)
         {
             Player p = new Player();
@@ -16,27 +98,36 @@ namespace TextGame
             int damagetaken;
             bool monsterdead = false;
             bool playerdead = false;
+            List<Room> RoomList = new List<Room>();
+            string filename = "D:/Rooms.txt";
 
-            while(monsterdead == false && playerdead == false)
+            parseRooms(filename, ref RoomList);
+
+            Console.WriteLine("The number of rooms created is: " + RoomList.Count);
+            for (int i = 0; i < RoomList.Count; i++)
             {
-            damagetaken = p.attack();
-            Console.WriteLine("Player attacks for: " + damagetaken);
-            monsterdead = boss.DamageFromPlayer(damagetaken);
-            if(monsterdead == true)
-            {
-                Console.WriteLine("Player won");
-                break;
+                Console.WriteLine("RoomName is: " + RoomList[i].RoomName);
             }
-            damagetaken = boss.generateDamage();
-            Console.WriteLine("Boss attack for: " + damagetaken);
-            playerdead = p.takeDamage(damagetaken);
-            if(playerdead == true)
-            {
-                Console.WriteLine("Player Lost");
-                break;
-            }
-            System.Threading.Thread.Sleep(3000);
-            }
+                //while (monsterdead == false && playerdead == false)
+                //{
+                //    damagetaken = p.attack();
+                //    Console.WriteLine("Player attacks for: " + damagetaken);
+                //    monsterdead = boss.DamageFromPlayer(damagetaken);
+                //    if (monsterdead == true)
+                //    {
+                //        Console.WriteLine("Player won");
+                //        break;
+                //    }
+                //    damagetaken = boss.generateDamage();
+                //    Console.WriteLine("Boss attack for: " + damagetaken);
+                //    playerdead = p.takeDamage(damagetaken);
+                //    if (playerdead == true)
+                //    {
+                //        Console.WriteLine("Player Lost");
+                //        break;
+                //    }
+                //    System.Threading.Thread.Sleep(3000);
+                //}
 
 
             Console.WriteLine("Curr HP IS: " + p.currHP);
---
+++ TextGame/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+    public class Room
+    {
+
+        public int id {get; set;}
+        public string RoomName { get; set; }
+        public string Description { get; set; }
+        public string otherItems { get; set; }
+        public int northid { get; set; }
+        public int southid { get; set; }
+        public int eastid { get; set; }
+        public int westid { get; set; }
+
+       
+    }
+}
